dbf1c303e965bd03d643188b8a6bcebf09a620be
appeltone <72234136+appeltone@users.noreply.github.com>
2020-10-05 18:59:02 +0300

    added 2 new methods: 1. showPreviousResultsFromDB 2. getDbConnection
    also added insert answers to results column in remote db to printResult method

--- src/calculator/UtilsClass.java
+++ src/calculator/UtilsClass.java
@@ -1,25 +1,64 @@
 package calculator;
 
+import com.mysql.cj.protocol.Resultset;
+
 import java.io.*;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class UtilsClass {
     final static String RESULT_FILE = "D:\\IdeaProjects\\src\\calculator\\Results.txt";
+    final static String RESULT_COLUMN = "results";
+
+    public static void showPreviousResultsFromDB() {
+        Connection con = getDbConnection();
+        try {
+            Statement st = con.createStatement();
+            ResultSet result = st.executeQuery("SELECT * FROM `calculator_db`");
+            while (result.next()) {
+                double resFromDb = result.getDouble("results");
+                System.out.println(resFromDb);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Connection getDbConnection() {
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/mCAjmMzagg", "mCAjmMzagg", "nJkeBH4czo");
+            //conn = DriverManager.getConnection("jdbc:postgresql://192.168.0.107:5432/dvdrental","postgres","cbr900rr!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
 
     public static void showPreviousResults() {
         try {
             File outPutFile = new File(RESULT_FILE); //create a new file object
-            if (!outPutFile.exists()){
+            if (!outPutFile.exists()) {
                 outPutFile.createNewFile(); //if file doesn't exist, create one
             }
             FileReader fileReader = new FileReader(outPutFile);   //read the file
             BufferedReader bufferedReader = new BufferedReader(fileReader);  //creates a buffering input stream to read line by line
-            StringBuffer stringBuffer =new StringBuffer();    //constructs a string buffer with no characters
+            StringBuffer stringBuffer = new StringBuffer();    //constructs a string buffer with no characters
 
             String line;
-            while((line=bufferedReader.readLine())!=null) //till end of file
+            while ((line = bufferedReader.readLine()) != null) //till end of file
             {
                 stringBuffer.append(line);      //appends line to string buffer
                 stringBuffer.append(System.lineSeparator());
@@ -61,6 +100,17 @@ public class UtilsClass {
             Date date = new Date(System.currentTimeMillis());
 
 
+            //print to results column in my remotemysql db
+            Connection con = getDbConnection();
+            try {
+                Statement st = con.createStatement();
+                st.execute("INSERT INTO `calculator_db` (`" + RESULT_COLUMN + "`) VALUES ('" + answer + "')");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+            //print to file
             Result result = new Result(String.valueOf(answer));
             printWriter.println(result.getValue());
             printWriter.close();
